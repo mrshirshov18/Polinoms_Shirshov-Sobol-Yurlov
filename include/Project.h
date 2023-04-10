26f5ab9ed77d9b729803cf2380d40a87ffb50724
BlackAngel2108 <luba.sobol.nn@mail.ru>
2023-04-10 11:58:59 +0300

    project

---
+++ include/Project.h
@@ -0,0 +1,13 @@
+//#include <iostream>
+//#include "Polinom.h"
+//#include "NoSortedTable1.h"
+//#include "NoSortedTable2.h"
+//#include "SortedTable.h"
+//#include "HashTable1.h"
+//#include "TreeTable.h"
+//#include "Postfix.h"
+//
+//extern "C" __declspec(dllexport)string f3(string num, string expression, string name);
+//extern "C" __declspec(dllexport)string f1(string pol1, string pol2, string Znak);
+//extern "C" __declspec(dllexport)string f2_in_point(string pol1, string x, string y, string z);
+//extern "C" __declspec(dllexport)string f2_const(string pol1, string constant);
